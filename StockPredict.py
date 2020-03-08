b924cf29c1802e2567c079a6e4b7c81f866ff8f8
Daniel <djnucci@hotmail.com>
2020-03-08 02:02:18 -0400

    Main functions added and the default test case
    Not certain this fits with the requirements, but I think it should work nicely. Hopefully this is a good starting point cause I'm decently tired and need a big nap

--- StockPredict.py
+++ StockPredict.py
@@ -1,8 +1,50 @@
+class Stock:
+    def __init__(self, name, shares, currSharePrice, prev4Days):
+        self.name = name
+        self.shares = shares
+        self.currSharePrice = currSharePrice
+        self.prev4Days = prev4Days
+
+# methods for buying and selling stocks
+    def buy(self, amount):
+        if amount < 0:
+            return
+
+        # only buy a stock if we have the money to
+        if m <= amount:
+            print(self.name + " BUY " + str(amount))
+
+    def sell(self, amount):
+        if amount < 0:
+            return
+
+        # only sell if you have enough stock
+        if self.shares <= amount:
+            print(self.name + " SELL " + str(amount))
+
+# this reads in the first line containing m (money for the day), k (number of stocks for the day) and d (number of remaining days to buy) values
 firstLine = input().rsplit()
 
-lines = []
+# variables allocated to 
+m = int(firstLine[0])
+k = int(firstLine[1])
+d = int(firstLine[2])
 
-for x in range(int(firstLine[1])):
+# using d, iterate through the days (not certian if needed, but it threw errors when I had it in)
+#for y in range(d):
+
+# using k, iterate through the lines
+lines = []
+for x in range(k):
     lines.append(input())
 
+# iterate through each line adding making an object for each stock
+stocks = []
+for x in range(k):
+    seperate = lines[x].rsplit()
+    #index 0 is name, 1 is shares owned, 6 is current stock price, 2 through 5 (inclusive) are the previous 4 days of stock price information
+    stocks.append(Stock(seperate[0], seperate[1], seperate[6], (seperate[2], seperate[3], seperate[4], seperate[5])))
 
+#buy usage
+stocks[0].buy(10)
+stocks[len(stocks) - 1].buy(3)
